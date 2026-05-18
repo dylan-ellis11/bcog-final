2712f5f909f4e593cb6eb395bb41b8f79e7b0154
dylan-ellis11 <dellis20@illinois.edu>
2026-05-17 21:29:41 -0500

    tests

--- data_draw.py
+++ data_draw.py
@@ -2,7 +2,9 @@ import os
 import shutil
 from medmnist import PneumoniaMNIST
 
-# 1. This triggers the download
+# AI Attribution: Used Gemini to explain how to implement using os/other libraries to avoid hardcoding file paths.
+# Prompt: "How can I implement the os library to avoid hardcoding the file path of my MedMNIST dataset?"
+# Based on Gemini's explanation and suggestion, I learned how to use os.path.join and what it does as well as shutil.copy.
 print("Downloading PneumoniaMNIST...")
 dataset = PneumoniaMNIST(split="train", download=True)
 
@@ -10,7 +12,7 @@ dataset = PneumoniaMNIST(split="train", download=True)
 original_filepath = os.path.join(dataset.root, "pneumoniamnist.npz")
 print(f"Downloaded to: {original_filepath}")
 
-# 3. Copies it directly to current project directory
+# 3. Copies it to current project directory
 current_directory = os.getcwd()
 new_filepath = os.path.join(current_directory, "pneumoniamnist.npz")
 
